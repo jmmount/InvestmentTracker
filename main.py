c734db4b3673e1e6ea69989e43a2df1d330be596
jmmount <eslnyc.jason@gmail.com>
2025-11-09 21:18:27 -0500

    commit pipeline chages

---
+++ main.py
@@ -0,0 +1,7 @@
+from orchestrators import stg_pipeline, wh_pipeline
+from orchestrators import stg_transaction_loader
+
+def main() :
+    stg_pipeline()
+if __name__ == "__main__":
+    main()
--- orchestrators/stg_pipeline.py
+++ orchestrators/stg_pipeline.py
@@ -1,10 +1,12 @@
-import sqlalchemy
+from sqlalchemy import text, create_engine
 import pandas as pd 
 
-csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRf1Xxc1wycc2YRWoIEnM7sWXbmKeZ74ZGbTbaE1bhNROHwMe3_5WaqozTQ2WnGed84RPB51bMQfBwb/pub?gid=1509911894&single=true&output=csv'
+def stg_pipeline() :
+    engine = create_engine('postgresql://admin:admin@localhost:5434/investments')
+    csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRf1Xxc1wycc2YRWoIEnM7sWXbmKeZ74ZGbTbaE1bhNROHwMe3_5WaqozTQ2WnGed84RPB51bMQfBwb/pub?gid=1509911894&single=true&output=csv'
+    stg_transaction_df = pd.read_csv(csv_url)
 
-try: 
-    stg_transaction  = pd.read_csv(csv)
-    print(stg_transaction.head(3))
-except Exception as e:
-    print("Error reading CSV:", e)
+    stg_transaction_df = stg_transaction_df.to_sql('stg_transaction', engine, if_exists='replace', index=False)
+
+if __name__ == "__main__":
+    stg_pipeline()
