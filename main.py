25e95c84e7ce7aee78564b69e842b62882d1f4c4
jmmount <eslnyc.jason@gmail.com>
2025-11-25 18:20:13 -0500

    refactors stagin pipeline to .py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from orchestrators import stg_pipeline, wh_pipeline
-from orchestrators import stg_transaction_loader
+from sqlalchemy import create_engine
+from orchestrators.stg_pipeline import stg_transaction
+from orchestrators.stg_pipeline import stg_history
+
+
+engine = create_engine('postgresql://admin:admin@localhost:5434/investments')
 
 def main() :
-    stg_pipeline()
+    stg_df = stg_transaction(engine)
+    stg_history(stg_df, engine)
+
 if __name__ == "__main__":
     main()
--- orchestrators/stg_pipeline.py
+++ orchestrators/stg_pipeline.py
@@ -1,12 +1,33 @@
-from sqlalchemy import text, create_engine
-import pandas as pd 
+import pandas as pd
+import yfinance as yf
 
-def stg_pipeline() :
-    engine = create_engine('postgresql://admin:admin@localhost:5434/investments')
-    csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRf1Xxc1wycc2YRWoIEnM7sWXbmKeZ74ZGbTbaE1bhNROHwMe3_5WaqozTQ2WnGed84RPB51bMQfBwb/pub?gid=1509911894&single=true&output=csv'
-    stg_transaction_df = pd.read_csv(csv_url)
+csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRf1Xxc1wycc2YRWoIEnM7sWXbmKeZ74ZGbTbaE1bhNROHwMe3_5WaqozTQ2WnGed84RPB51bMQfBwb/pub?gid=1509911894&single=true&output=csv'
 
-    stg_transaction_df = stg_transaction_df.to_sql('stg_transaction', engine, if_exists='replace', index=False)
+
+def stg_transaction(engine):
+    df = pd.read_csv(csv_url)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.to_sql('stg_transaction', engine, if_exists='replace', index=False)
+    print("stg_transaction loaded successfully!")
+    return df
+
+def stg_history(df, engine):
+    tickers = df['Ticker'].dropna().unique().tolist()
+    history = yf.download(tickers, start="2024-01-02", interval="1wk", group_by='Ticker')
+    
+    records = []
+    for ticker in tickers:
+        ticker_df = history[ticker].copy()
+        ticker_df = ticker_df.reset_index()
+        ticker_df['Ticker'] = ticker
+        records.append(ticker_df)
+
+    stg_history_df = pd.concat(records, ignore_index=True)
+    stg_history_df.rename(columns=lambda x: x.replace(" ", "_"), inplace=True)
+    stg_history_df.to_sql('stg_history', engine, if_exists='replace', index=False)
+    print("stg_history loaded successfully!")
+    return stg_history_df
 
 if __name__ == "__main__":
-    stg_pipeline()
+    stg_transaction()
+    stg_history()
